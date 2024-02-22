9baa53d28c7e1183c86ce81391235f2672c02b8b
Vishesh17 <visheshgpt17@gmail.com>
2024-02-23 00:12:56 +0530

    Final Commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,50 +1,47 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
-const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
-
+const User = require("../models/userModel");
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
-      res.status(400);
-      throw new Error("All fields are mandatory!");
-    }
-    const userAvailable = await User.findOne({ email });
-    if (userAvailable) {
-      res.status(400);
-      throw new Error("User already registered!");
-    }
-  
-    //Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log("Hashed Password: ", hashedPassword);
-    const user = await User.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
-  
-    console.log(`User created ${user}`);
-    if (user) {
-      res.status(201).json({ _id: user.id, email: user.email });
-    } else {
-      res.status(400);
-      throw new Error("User data is not valid");
-    }
-    res.json({ message: "Register the user" });
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already registered!");
+  }
+
+  //Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("Hashed Password: ", hashedPassword);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
   });
 
+  console.log(`User created ${user}`);
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
+  res.json({ message: "Register the user" });
+});
 
-//@desc Login a user
+//@desc Login user
 //@route POST /api/users/login
 //@access public
-
-const loginUser = asyncHandler(async(req,res)=>{
-    const { email, password } = req.body;
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
   if (!email || !password) {
     res.status(400);
     throw new Error("All fields are mandatory!");
@@ -68,15 +65,13 @@ const loginUser = asyncHandler(async(req,res)=>{
     res.status(401);
     throw new Error("email or password is not valid");
   }
-}) 
+});
 
-//@desc  Current user
-//@route GET /api/users/login
-//@access private 
+//@desc Current user info
+//@route POST /api/users/current
+//@access private
+const currentUser = asyncHandler(async (req, res) => {
+  res.json(req.user);
+});
 
-const currentUser = asyncHandler(async(req,res)=>{
-    res.json(req.user);
-})
-
-
-module.exports = {registerUser , loginUser, currentUser};
+module.exports = { registerUser, loginUser, currentUser };
